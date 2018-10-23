9f38190046e5dad5ca9bcd2cbec010ba65f4f919
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2018-10-23 11:34:42 -0500

    show ast by pycparser.

---
+++ gcc-torture/scripts/show_ast_pycparser.py
@@ -0,0 +1,14 @@
+import sys
+from pycparser import c_parser
+
+src_file = sys.argv[1]
+if (not src_file.endswith(".c")):
+    sys.exit()
+
+text_code = ""
+with open(src_file) as rd_file:
+    text_code = rd_file.read()
+
+parser = c_parser.CParser()
+ast = parser.parse(text_code, filename='<none>')
+ast.show()
