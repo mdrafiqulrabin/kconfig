a5bae9bc20b2624d408ace92042b6dd77e42230c
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2018-10-25 02:49:39 -0500

    dump_gcov-stats-single_decimal

--- gcc-torture/dump-gcov/gcov_stats_single/dump_gcov-stats-single_decimal.py
+++ gcc-torture/dump-gcov/gcov_stats_single/dump_gcov-stats-single_decimal.py
@@ -6,9 +6,9 @@ import sys, os
 ### parameters ###
 
 path_all_c_gcov_files = "gcov_stats_all"
-#path_all_c_gcov_files = "gcov_stats_temp"
+#path_all_c_gcov_files = "gcov_stats_temp" #temp
 
-output_file = "gcov-stats-single.tsv"
+output_file = "gcov-stats-single_decimal.tsv"
 
 ### methods ###
 
@@ -17,6 +17,7 @@ def getBinary2d(in_file):
     with open (in_file, 'r') as fin:
         for line in fin:
             binary_1d = []
+            #line = [x for x in line.strip().split(" ") if len(x.strip())>0] #temp
             line = [x for x in line.strip().split("\t") if len(x.strip())>0]
             for binary in line:
                 binary_1d.append(int(binary))
@@ -24,7 +25,6 @@ def getBinary2d(in_file):
     return binary_2d
 
 def getTsvData(binary_2d):
-    #print binary_2d
     final_data = ""
     for i in binary_2d:
         temp_data = ""
@@ -46,21 +46,20 @@ print "Start: ..."
 os.remove(output_file) if os.path.exists(output_file) else None
 all_c_gcov_files = os.listdir(path_all_c_gcov_files)
 
-#result_binary_2d = getBinary2d(path_all_c_gcov_files + "/t0.tsv")
+#result_binary_2d = getBinary2d(path_all_c_gcov_files + "/t0.tsv") #temp
 result_binary_2d = getBinary2d("gcov_stats_zero.tsv")
-#print result_binary_2d
 
 for c_gcov_file in all_c_gcov_files:
     c_gcov_file = path_all_c_gcov_files + "/" + c_gcov_file
     #ignore problematic input
     if (not c_gcov_file.endswith(".tsv")): continue
     if (c_gcov_file.endswith("pr58088-gcov.tsv")): continue
+    print c_gcov_file
     temp_binary_2d = getBinary2d(c_gcov_file)
-    #print temp_binary_2d
     
     for i, binary_1d in enumerate(temp_binary_2d):
         for j, binary in enumerate(binary_1d):
-            result_binary_2d[i][j] |= temp_binary_2d[i][j] #OR
+            result_binary_2d[i][j] += temp_binary_2d[i][j] #ADD
 
 saveToFile(output_file, getTsvData(result_binary_2d))
 
