0388dfc0394afd7cc6b13706ef6d6b5de36f68d8
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2018-10-25 01:26:40 -0500

    gcov_stats_single

---
+++ gcc-torture/dump-gcov/gcov_stats_single/dump_gcov-stats-single.py
@@ -0,0 +1,67 @@
+# x-axis: all gcov files and y-axis: line number of gcov file
+# [x][y] gcov stats for all c files
+
+import sys, os
+
+### parameters ###
+
+path_all_c_gcov_files = "gcov_stats_all"
+#path_all_c_gcov_files = "gcov_stats_temp"
+
+output_file = "gcov-stats-single.tsv"
+
+### methods ###
+
+def getBinary2d(in_file):
+    binary_2d = []
+    with open (in_file, 'r') as fin:
+        for line in fin:
+            binary_1d = []
+            line = [x for x in line.strip().split("\t") if len(x.strip())>0]
+            for binary in line:
+                binary_1d.append(int(binary))
+            binary_2d.append(binary_1d)
+    return binary_2d
+
+def getTsvData(binary_2d):
+    #print binary_2d
+    final_data = ""
+    for i in binary_2d:
+        temp_data = ""
+        for j in i:
+            if (len(temp_data)>0): temp_data += "\t"
+            temp_data += str(j)
+        final_data += temp_data + "\n"
+    return final_data
+
+def saveToFile(out_file, tmp_data):
+    with open (out_file, 'w') as fout:
+        fout.write(tmp_data)
+        fout.write('\n')
+
+### main ###
+
+print "Start: ..."
+
+os.remove(output_file) if os.path.exists(output_file) else None
+all_c_gcov_files = os.listdir(path_all_c_gcov_files)
+
+#result_binary_2d = getBinary2d(path_all_c_gcov_files + "/t0.tsv")
+result_binary_2d = getBinary2d("gcov_stats_zero.tsv")
+#print result_binary_2d
+
+for c_gcov_file in all_c_gcov_files:
+    c_gcov_file = path_all_c_gcov_files + "/" + c_gcov_file
+    #ignore problematic input
+    if (not c_gcov_file.endswith(".tsv")): continue
+    if (c_gcov_file.endswith("pr58088-gcov.tsv")): continue
+    temp_binary_2d = getBinary2d(c_gcov_file)
+    #print temp_binary_2d
+    
+    for i, binary_1d in enumerate(temp_binary_2d):
+        for j, binary in enumerate(binary_1d):
+            result_binary_2d[i][j] |= temp_binary_2d[i][j] #OR
+
+saveToFile(output_file, getTsvData(result_binary_2d))
+
+print "Done: ..."
--- gcc-torture/dump-gcov/readme.txt
+++ gcc-torture/dump-gcov/readme.txt
@@ -7,13 +7,16 @@ gcov-stats-all:
     0 = This c file didn't cover the y line of x gcov file
 
 gcov-stats-single.tsv:
-    - gcov status of all c files
-    - x axis contains c files
-    - y axis contains line number of all gcov files one by one
+    - gcov status of all c file
+    - x axis contains gcov files
+    - y axis contains line number of gcov file
+
+    1 = Atleast one c file covered the y line of x gcov file
+    0 = No c file covered the y line of x gcov file
 
 order_c_files.txt:
     - Order of c files in x axis of gcov-stats-single.tsv
 
 order_gcov_files.txt:
-    - Order of gcov files in y axis of gcov-stats-single.tsv
+    - Order of gcov files in x axis of gcov-stats-single.tsv
     - Order of gcov files in x axis of gcov-stats-all
