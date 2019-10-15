dba479c21a94b5533ed50ac67f876c6445c04e7b
mdrafiqulrabin <mdrafiqulrabin@gmail.com>
2019-10-15 17:08:18 -0500

    centroids with xmeans

---
+++ todo/scripts/kconfig/1_xmeans_centroids_prob.py
@@ -0,0 +1,48 @@
+import pandas as pd, numpy as np, random
+from pyclustering.cluster.xmeans import xmeans
+from pyclustering.cluster.center_initializer import kmeans_plusplus_initializer
+
+#read sample
+fname = "../../data/args/args_norm.csv"
+df_args = pd.read_csv(fname)
+df_args = df_args.drop('i', axis=1)
+df_args = df_args.drop('cfile', axis=1)
+df_args = df_args.drop('main', axis=1)
+sample = df_args.values.tolist()
+print("sample = ", len(sample))
+
+#parameters
+n_max_itr = 1000
+
+#iterative df_centroids
+m_len_centroids = len(sample)
+columns_name = df_args.columns.tolist()
+df_centroids = pd.DataFrame(columns = columns_name)
+
+itr=0
+while itr < n_max_itr:
+    itr = itr + 1
+    print("\n[ itr = ", itr, " ]")
+    
+    #process xmeans
+    amount_initial_centers = len(columns_name)
+    amount_max_centers = int(len(sample)/amount_initial_centers)
+    initial_centers = kmeans_plusplus_initializer(sample, amount_initial_centers).initialize()
+    xmeans_instance = xmeans(sample, initial_centers, amount_max_centers)
+    xmeans_instance.process()
+    clusters = xmeans_instance.get_clusters()
+    centers = xmeans_instance.get_centers()
+    print("centers = ", len(centers))
+
+    #filter clusters
+    df_temp = pd.DataFrame(np.array(centers))
+    df_temp.columns = columns_name
+    if (m_len_centroids > len(df_temp)):
+        df_centroids = df_temp
+        df_centroids = df_centroids.drop_duplicates(keep='first')
+    print("centroids = ", len(df_centroids))
+
+#write centroids
+fname = "../../data/args/args_centroids_prob.csv"
+df_centroids = df_centroids.loc[(df_centroids!=0).any(axis=1)]
+df_centroids.to_csv(fname, sep=',', encoding='utf-8', index=False)
